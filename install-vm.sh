1f9f8bcfb97b0ff1c2098dcfc10c12c61fb1a43b
ZrH-Source <71234118+ZrH-Source@users.noreply.github.com>
2022-05-27 14:32:51 +0200

    Using Ansible with bash

--- install-vm.sh
+++ install-vm.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-read -p " Enter your host (default : 192.168.122.53 ) : " host
-host=${host:-'192.168.122.53'}
-read -p "Enter your username (default : root) :" user
-user=${user:-'root'}
-read -s -p "Enter your password : " password
-
-echo "Downloading the latest version of sshpass"
-sudo apt install sshpass
+echo 'Installing ansible.'
 sudo apt update
-echo 'Connecting to your host on ssh and creating the virtual machine'
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible 
 
-id=${1:-'100'}
-name=${2:-'debian'}
-memory=${3:-'2048'}
-socket=${4:-'2'}
-iso=${5:-'debian-11.3.0-amd64-netinst.iso'}
-
-sshpass -p ${password} ssh ${user}@${host} 'qm create '${id}' --name '${name}' --cdrom local:iso/'${iso}' --memory '${memory}' --socket '${socket}
+echo 'Running Ansible playbook.'
+sudo ansible-playbook -i hosts.ini install-vm.yml -e @extra-var.yml
