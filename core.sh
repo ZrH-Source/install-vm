302c2c7c6c0d2b9d9265238a9e27f3506b74de8f
ZrH-Source <zariochgta@gmail.com>
2022-06-28 16:58:57 +0200

    updated version of core.sh

--- core.sh
+++ core.sh
@@ -19,12 +19,12 @@ iso=${iso:-'debian-live-11.3.0-amd64-gnome.iso'}
 for id in $@; do :; done # Last args
 
 echo 'Running Ansible playbook.'
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts.ini install-vm.yml -e @extra_vars.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts.ini install-vm.yml -e @extra_vars.yml # Create and start the virtual machine with the options in extra_vars.yml
 
 echo 'Waiting for the vm to start.'
 echo ''
 chown root:root /tmp/ips.txt
 sleep 60
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts.ini retrieve_ip.yml -e @extra_vars.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts.ini retrieve_ip.yml -e @extra_vars.yml # Grab the Ip with an arp -a in docker
 echo 'Stopping the vm.'
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts.ini stopping-vm.yml -e @extra_vars.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts.ini stopping-vm.yml -e @extra_vars.yml # Stop the vm 
